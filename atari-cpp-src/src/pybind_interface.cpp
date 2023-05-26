3bcfe8724d6269655245be8e3373c42e60f58a7f
Haseeb Shah <haseebs@outlook.com>
2023-05-26 14:21:25 -0700

    adjust interface

--- atari-cpp-src/src/pybind_interface.cpp
+++ atari-cpp-src/src/pybind_interface.cpp
@@ -2,11 +2,19 @@
 #include <pybind11/stl.h>
 #include <vector>
 #include "../include/agent_interface.h"
+//#include "../include/ale_interface.hpp"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(RealtimeAtari, m) {
-    py::class_<RealTimeAtari>(m, "RealTimeAtari")
+    py::class_<Environment>(m, "Environment")
+        //.def(py::init<>())
+        .def("sense", &Environment::sense)
+        .def("get_features", &Environment::get_features)
+        .def("get_reward", &Environment::get_reward)
+        .def("act", &Environment::act);
+
+    py::class_<RealTimeAtari, Environment>(m, "RealTimeAtari")
         .def(py::init<>())
         .def("sense", &RealTimeAtari::sense)
         .def("get_features", &RealTimeAtari::get_features)
