63c1740487476372f7d17692443731e5e0a82446
Haseeb Shah <haseebs@outlook.com>
2023-05-26 15:11:33 -0700

    update example

--- atari-cpp-src/example.py
+++ atari-cpp-src/example.py
@@ -1,7 +1,10 @@
 from RealtimeAtari import RealTimeAtari
+import time
 
 env = RealTimeAtari()
 env.get_features()
 env.get_reward()
 env.sense()
+for i in range(2000000000):
+    i ** 2
 env.act(1)
